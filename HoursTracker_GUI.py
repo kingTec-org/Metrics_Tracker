17d9d5a1824deb423aa7cb04ea7e57a9fc334c58
Larry Cawley <LarryDCJ@icloud.com>
2020-10-09 22:00:58 +0100

    Showing Grid layout with correct format
    Tighten up UI
    Decide what buttons/tabs need to be added
    Don't break any other functionality

--- HoursTracker_GUI.py
+++ HoursTracker_GUI.py
@@ -10,11 +10,9 @@ class Window(QMainWindow):
 		super(Window, self).__init__()
 		self.setGeometry(50, 50, 500, 500)
 		self.setWindowTitle("Aircraft Hours Tracker")
-		self.grid_layout = QGridLayout()
 
-		extract_action = QAction("Get to the chopper!!!", self)
+		extract_action = QAction("Exit", self)
 		extract_action.setShortcut("Ctrl+Q")
-		extract_action.setStatusTip("Leave The App")
 		extract_action.triggered.connect(self.close_application)
 
 		self.statusBar()
@@ -23,51 +21,54 @@ class Window(QMainWindow):
 		file_menu = main_menu.addMenu('&File')
 		file_menu.addAction(extract_action)
 
+		self.grid = QGridLayout()
+		w = QWidget()
+		w.setLayout(self.grid)
+		self.setCentralWidget(w)
+
 		self.home()
 
 	def home(self):
+
 		quit_btn = QPushButton("Quit", self)
+		self.grid.addWidget(quit_btn, 0, 0)
 		quit_btn.clicked.connect(self.close_application)
-		self.grid_layout.addWidget(quit_btn, 0, 0)
 
 		crew_btn = QPushButton("Add New Crew-member", self)
+		self.grid.addWidget(crew_btn, 0, 1)
+
 		crew_btn.clicked.connect(self.add_crew_member)
-		self.grid_layout.addWidget(crew_btn, 0, 1)
 
 		global enter_employee_id
 		enter_employee_id = QLineEdit(self)
+		self.grid.addWidget(enter_employee_id, 0, 2)
 		enter_employee_id.setPlaceholderText("Enter Employee ID#")
 		enter_employee_id.setMaxLength(8)
-		self.grid_layout.addWidget(enter_employee_id, 0, 2)
 
 		global enter_first_name
 		enter_first_name = QLineEdit(self)
+		self.grid.addWidget(enter_first_name, 0, 3)
 		enter_first_name.setPlaceholderText("Enter First Name")
 		enter_first_name.setMaxLength(32)
-		efn = enter_first_name.text()
-		self.grid_layout.addWidget(enter_first_name, 0, 3)
 
 		global enter_middle_name
 		enter_middle_name = QLineEdit(self)
+		self.grid.addWidget(enter_middle_name, 0, 4)
 		enter_middle_name.setPlaceholderText("Enter Middle Name")
 		enter_middle_name.setMaxLength(32)
-		emn = enter_middle_name.text()
-		self.grid_layout.addWidget(enter_middle_name, 0, 4)
 
 		global enter_last_name
 		enter_last_name = QLineEdit(self)
+		self.grid.addWidget(enter_last_name, 0, 5)
 		enter_last_name.setPlaceholderText("Enter Last Name")
 		enter_last_name.setMaxLength(32)
-		eln = enter_last_name.text()
-		self.grid_layout.addWidget(enter_last_name, 0, 5)
 
-		# TODO: Turn this into a drop-down box to select crew positions and ratings
+		## TODO: Turn this into a drop-down box to select crew positions and ratings
 		global enter_crew_position
 		enter_crew_position = QLineEdit(self)
+		self.grid.addWidget(enter_crew_position, 0, 6)
 		enter_crew_position.setPlaceholderText("Select Crew Position")
 		enter_crew_position.setMaxLength(16)
-		ecp = enter_crew_position.text()
-		self.grid_layout.addWidget(enter_crew_position, 0, 6)
 
 		self.show()
 
