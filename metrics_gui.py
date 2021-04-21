067a8597e3dcd21ce50713cee2c5432a4c9f7d92
Larry Cawley <larrydcj@icloud.com>
2021-04-21 01:05:00 -0700

    eh

--- metrics_gui.py
+++ metrics_gui.py
@@ -14,6 +14,8 @@ def main_window():
         [sg.Button('Exit', size=(22, 1), key='-EXIT-')]]
 
     return sg.Window('Metrics', layout, finalize=True)
+
+
 def main_view_crew_window():
     crew_info_values = get_crew_query()
     headings = get_crew_column_query()
@@ -34,14 +36,20 @@ def main_view_crew_window():
               [sg.Button('Back', size=(10, 1), key='-BACK-')]
               ]
     return sg.Window('Aircrew', layout, finalize=True)
+
+
 def crew_details_window():
     layout = [
         [sg.Button('Back', size=(10, 1), key='-BACK-')]]
     return sg.Window('Crew Details', layout, finalize=True)
+
+
 def edit_crew_window():
     layout = [
         [sg.Button('Back', size=(10, 1), key='-BACK-')]]
     return sg.Window('Edit Crew', layout, finalize=True)
+
+
 def add_crew_window():
     gender = ['male', 'female']
     suffix = ['Jr.', 'Sr.', 'III', 'IV']
@@ -56,6 +64,8 @@ def add_crew_window():
         [sg.Text('Crew Position', size=(15, 1)), sg.InputText(f'{random.choice(crew_pos)}')],
         [sg.Submit(size=(7, 1), key='-SUBMIT-'), sg.Button('Back', size=(7, 1), key='-BACK-')]]
     return sg.Window('Add Crew', layout, finalize=True)
+
+
 def main_view_site_window():
     site_info_values = get_site_query()
     headings = get_site_column_query()
@@ -75,14 +85,20 @@ def main_view_site_window():
                         )],
               [sg.Button('Back', size=(10, 1), key='-BACK-')]]
     return sg.Window('Sites', layout, finalize=True)
+
+
 def site_details_window():
     layout = [
         [sg.Button('Back', size=(10, 1), key='-BACK-')]]
     return sg.Window('Site Details', layout, finalize=True)
+
+
 def edit_site_window():
     layout = [
         [sg.Button('Back', size=(10, 1), key='-BACK-')]]
     return sg.Window('Edit Site', layout, finalize=True)
+
+
 def add_site_window():
     layout = [
         [sg.Text('Site ID', size=(15, 1)), sg.InputText('')],
@@ -93,45 +109,83 @@ def add_site_window():
         [sg.Submit(size=(10, 1), key='-SUBMIT-'), sg.Button('Back', size=(10, 1), key='-BACK-')]]
     return sg.Window('Add Site', layout, finalize=True)
 
+
+def view_specific_crew():
+    # get site name from table
+    window_heading = ''
+    site_info_values = get_site_query()
+    headings = get_site_column_query()
+    layout = [[sg.Button('View Site', size=(10, 1), key='-VIEW SITE-'),
+               sg.Button('Edit Site', size=(10, 1), key='-EDIT SITE-'),
+               sg.Button('Add Site', size=(10, 1), key='-ADD SITE-'),
+               sg.Button('Delete Site', size=(10, 1), key='-DELETE SITE-')],
+              [sg.Table(values=site_info_values,
+                        headings=headings,
+                        auto_size_columns=True,
+                        display_row_numbers=True,
+                        justification="left",
+                        alternating_row_color='LightGray',
+                        enable_events=True,
+                        bind_return_key=False,
+                        key='-READ TABLE-'
+                        )],
+              [sg.Button('Back', size=(10, 1), key='-BACK-')]]
+    return sg.Window(f'{window_heading}', layout, finalize=True)
+
+
+def view_specific_site():
+    pass
+
+
 display_main_window = main_window()
 
 display_main_view_crew_window = None
 display_detail_crew_window = None
 display_edit_crew_window = None
 display_add_crew_window = None
-
+display_view_specific_crew_window = None
 
 display_main_view_site_window = None
 display_detail_site_window = None
 display_edit_site_window = None
 display_add_site_window = None
+display_view_specific_site_window = None
 
 # window loop
 while True:
     window, event, values = sg.read_all_windows()
+    print(event, values)
+
+    ##-----------------MAIN WINDOW------------------##
 
     # main_window
     if window == display_main_window:
-        if event in (sg.WINDOW_CLOSE_ATTEMPTED_EVENT, sg.WIN_CLOSED, '-EXIT-'):
+
+        if event in (sg.WIN_CLOSED, '-EXIT-'):
             break
         elif event == '-VIEW CREW WINDOW-':
+            display_main_window.close()
             display_main_view_crew_window = main_view_crew_window()
-            display_main_window.Hide()
         elif event == '-VIEW SITE WINDOW-':
+            display_main_window.close()
             display_main_view_site_window = main_view_site_window()
-            display_main_window.Hide()
+
+    ##-----------MAIN VIEW WINDOWS---------------##
 
     # main_view_crew_window
     if window == display_main_view_crew_window:
-        if event in (sg.WINDOW_CLOSE_ATTEMPTED_EVENT, sg.WIN_CLOSED, '-BACK-'):
-            display_main_window.UnHide()
+        if event in (sg.WIN_CLOSED, '-BACK-'):
+            display_main_view_crew_window.refresh()
             display_main_view_crew_window.close()
+            display_main_window = main_window()
         elif event == '-VIEW CREW-':
+            display_main_view_crew_window.refresh()
+            display_main_view_crew_window.close()
             display_detail_crew_window = crew_details_window()
-            display_main_view_crew_window.close()
         elif event == '-ADD CREW-':
-            display_add_crew_window = add_crew_window()
+            display_main_view_crew_window.refresh()
             display_main_view_crew_window.close()
+            display_add_crew_window = add_crew_window()
         elif event == '-DELETE CREW-':
             for idx in values['-READ TABLE-']:
                 crew_info = get_crew_query()
@@ -143,23 +197,25 @@ while True:
 
                 # actual mongo delete command
                 delete_crew_member(employee_id)
+            display_main_view_crew_window.refresh()
             display_main_view_crew_window.close()
             display_view_crew_window = main_view_crew_window()
         elif event == '-EDIT CREW-':
-            display_edit_crew_window = edit_crew_window()
+            display_main_view_crew_window.refresh()
             display_main_view_crew_window.close()
+            display_edit_crew_window = edit_crew_window()
 
     # main_view_site_window
     if window == display_main_view_site_window:
-        if event in (sg.WINDOW_CLOSE_ATTEMPTED_EVENT, sg.WIN_CLOSED, '-BACK-'):
-            display_main_window.UnHide()
+        if event in (sg.WIN_CLOSED, '-BACK-'):
             display_main_view_site_window.close()
+            display_main_window = main_window()
         elif event == '-VIEW SITE-':
+            display_main_view_site_window.close()
             display_detail_site_window = site_details_window()
-            display_main_view_site_window.close()
         elif event == '-ADD SITE-':
-            display_add_site_window = add_site_window()
             display_main_view_site_window.close()
+            display_add_site_window = add_site_window()
         elif event == '-DELETE SITE-':
             for idx in values['-READ TABLE-']:
                 site_info = get_site_query()
@@ -174,55 +230,81 @@ while True:
             display_main_view_site_window.close()
             display_main_view_site_window = main_view_site_window()
         elif event == '-EDIT SITE-':
-            display_edit_site_window = edit_site_window()
             display_main_view_site_window.close()
+            display_edit_site_window = edit_site_window()
+
+    ##----------DISPLAY DETAILS WINDOWS------------##
 
     # display_crew_details_window
     if window == display_detail_crew_window:
-        if event in (sg.WINDOW_CLOSE_ATTEMPTED_EVENT, sg.WIN_CLOSED, '-BACK-'):
-            display_view_crew_window = main_view_crew_window()
+        if event in (sg.WIN_CLOSED, '-BACK-'):
             display_detail_crew_window.close()
+            display_view_crew_window = main_view_crew_window()
 
     # display_site_details_window
     if window == display_detail_site_window:
-        if event in (sg.WINDOW_CLOSE_ATTEMPTED_EVENT, sg.WIN_CLOSED, '-BACK-'):
-            display_main_view_site_window = main_view_site_window()
+        if event in (sg.WIN_CLOSED, '-BACK-'):
             display_detail_site_window.close()
+            display_main_view_site_window = main_view_site_window()
+
+    ##--------------EDIT WINDOWS---------------##
 
     # edit_crew_window
     if window == display_edit_crew_window:
-        if event in (sg.WINDOW_CLOSE_ATTEMPTED_EVENT, sg.WIN_CLOSED, '-BACK-'):
-            display_view_crew_window = main_view_crew_window()
+        if event in (sg.WIN_CLOSED, '-BACK-'):
             display_edit_crew_window.close()
+            display_main_view_crew_window = main_view_crew_window()
 
     # edit_site_window
     if window == display_edit_site_window:
-        if event in (sg.WINDOW_CLOSE_ATTEMPTED_EVENT, sg.WIN_CLOSED, '-BACK-'):
+        if event in (sg.WIN_CLOSED, '-BACK-'):
+            display_edit_site_window.close()
             display_main_view_site_window = main_view_site_window()
-            display_edit_site_window.close()
-        elif event == '-ADD SITE WINDOW-':
-            display_add_site_window = add_site_window()
-            display_edit_site_window.close()
+
+
+    ##------------ADD WINDOWS---------------##
 
     # add_crew_window
     if window == display_add_crew_window:
-        if event in (sg.WINDOW_CLOSE_ATTEMPTED_EVENT, sg.WIN_CLOSED, '-BACK-'):
-            display_view_crew_window = main_view_crew_window()
+        if event in (sg.WIN_CLOSED, '-BACK-'):
+            display_add_crew_window.refresh()
             display_add_crew_window.close()
+            display_main_view_crew_window = main_view_crew_window()
         elif event == '-SUBMIT-':
+            display_add_crew_window.refresh()
             add_crew_members(values)
-            display_view_crew_window = main_view_crew_window()
             display_add_crew_window.close()
+            display_main_view_crew_window = main_view_crew_window()
 
     # add_site_window
     if window == display_add_site_window:
-        if event in (sg.WINDOW_CLOSE_ATTEMPTED_EVENT, sg.WIN_CLOSED, '-BACK-'):
-            display_main_view_site_window = main_view_site_window()
+        if event in (sg.WIN_CLOSED, '-BACK-'):
             display_add_site_window.close()
+            display_main_view_site_window = main_view_site_window()
         elif event == '-SUBMIT-':
             add_site(values)
-            display_main_view_site_window = main_view_site_window()
             display_add_site_window.close()
+            display_main_view_site_window = main_view_site_window()
+
+    ##------------VIEW SPECIFICS WINDOWS--------------##
+
+    #
+    if window == display_view_specific_site_window:
+        if event in (sg.WIN_CLOSED, '-BACK-'):
+            display_view_specific_site_window.close()
+            display_main_view_site_window = main_view_site_window()
+        elif event == '-EDIT SITE-':
+            display_main_view_site_window.close()
+            display_edit_site_window = edit_site_window()
+
+    #
+    if window == display_view_specific_crew_window:
+        if event in (sg.WIN_CLOSED, '-BACK-'):
+            display_main_view_crew_window = main_view_crew_window()
+            display_view_specific_crew_window.close()
+        elif event == '-EDIT CREW-':
+            display_edit_crew_window = edit_crew_window()
+            display_main_view_crew_window.close()
 
 # end of program
 window.close()
